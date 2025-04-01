6798aa5e0ba3911a13e7ad396caf111666fba218
wg-song <67891037+wg-song@users.noreply.github.com>
2025-04-01 16:05:12 +0900

    initial commit
    Add Libraries and configuration files.

---
+++ config/connection_details.r
@@ -0,0 +1,21 @@
+dbms <- ""
+user <- ""
+password <- ""
+server <- "127.0.0.1/local"
+
+cdmDatabaseSchema <- "schema"
+cdmDatabaseName <- "database"
+oracleTempSchema <- "temp"
+tempEmulationSchema <- "temp"
+cohortDatabaseSchema <- "cohort_schema"
+cohortTable <- "cohort"
+exposureDatabaseSchema <- "exposure"
+outcomeDatabaseSchema <- "outcome_schema"
+outcomeTable <- "cohort"
+vocaSchema <- "voca"   
+
+targetId <- 1000
+outcomeIds <- 1001
+cdmVersion <- 5
+
+seed <- 1000
